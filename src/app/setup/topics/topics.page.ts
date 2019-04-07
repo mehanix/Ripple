0846bbe67e85a6b3ced789557aeca17d388273a6
Nicoleta Ciausu <nixikunn@gmail.com>
2019-04-07 22:08:00 +0300

    firebase shenanigans'

--- src/app/setup/topics/topics.page.ts
+++ src/app/setup/topics/topics.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { nume: string; }
+
 
 @Component({
   selector: 'app-topics',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicsPage implements OnInit {
 
+  private topicsCollection:AngularFirestoreDocument<Item>;
+  private topic:Observable<Item>;
 
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { 
+
+    this.topicsCollection = firestore.doc<any>('topics/caini');
+    this.topic = this.topicsCollection.valueChanges();
+    this.topic.subscribe(res=>{
+      console.log(res.nume);
+  });
+      
+    
+  
+
+   //this.topics.forEach(element => { console.log(element);
+      
+   // });
+    
+  }
 
   ngOnInit() {
   }
