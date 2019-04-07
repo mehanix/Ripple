af1fd3bd5906d3caefb16a98daca67f60eae1382
Nicoleta Ciausu <nixikunn@gmail.com>
2019-04-07 20:47:23 +0300

    set up Capacitor, done test build. setting up firebase, WIP. fixed git, oh god

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { firebaseConfig } from '../credentials';
+
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+            IonicModule.forRoot(),
+            AngularFireModule.initializeApp(firebaseConfig),
+            AngularFirestoreModule,AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBtybh44ZkY4m06035ggap_lMzT7DsAu5I",
+    authDomain: "ripple-ec3e.firebaseapp.com",
+    databaseURL: "https://ripple-ec3e.firebaseio.com",
+    projectId: "ripple-ec3e",
+    storageBucket: "ripple-ec3e.appspot.com",
+    messagingSenderId: "150067106150"
+  };
