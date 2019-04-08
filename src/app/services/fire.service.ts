d70d4af589b0c3c7413b12a4451a977226360466
Nicoleta Ciausu <nixikunn@gmail.com>
2019-04-08 22:31:05 +0300

    more work on db, switched from valueChanges() to snapshot()

--- src/app/services/fire.service.ts
+++ src/app/services/fire.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFirestoreCollection, AngularFirestoreDocument, DocumentChangeAction } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export interface Item { 
   nume: string;
   imagine: string;
   desc: string;
+  id: string;
 }
 
 
@@ -23,7 +25,13 @@ export class FireService {
 
     /** array cu informatiile de prezentare ale topic-urilor **/
     this.topicsPrezentareCollection = firestore.collection<Item>('prezentare');
-    this.topicsPrezentare = this.topicsPrezentareCollection.valueChanges();
+    this.topicsPrezentare = this.topicsPrezentareCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
 
   }
 
