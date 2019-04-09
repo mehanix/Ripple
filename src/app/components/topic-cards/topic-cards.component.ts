382a2cc9314af70868cf9b5b2b06e427856de5cb
Nicoleta Ciausu <nixikunn@gmail.com>
2019-04-09 22:51:35 +0300

    loading from db complete! selecting topics complete! and some ui fixes of course

--- src/app/components/topic-cards/topic-cards.component.ts
+++ src/app/components/topic-cards/topic-cards.component.ts
@@ -9,14 +9,54 @@ import { FireService } from "../../services/fire.service";
 export class TopicCardsComponent implements OnInit {
 
   private topics;
-
+  private slides;
+  private selectedTopics;
+  private buttonIcon:string;
 
   constructor(fireService: FireService) {
 
     this.topics = fireService.getTopicsPrezentare();
+    this.selectedTopics = [];
+    this.buttonIcon="heart-empty";
 
    }
 
-  ngOnInit() {}
+
+   
+  ngOnInit() {
+
+  }
+
+   cardClick(button:HTMLIonButtonElement){
+
+    /** in layoutul card-ului, titlul este elementul nr 3 **/
+    var card = button.parentElement.parentElement.parentElement;
+
+    var text = card.children.item(1).children.item(1).textContent;
+
+    /** verif daca exista deja valoarea in array **/
+    var index = this.selectedTopics.indexOf(text);
+
+    if (index === -1)  {
+      /** adauga textul in array, marcheaza card-ul ca si clicked **/
+      this.selectedTopics.push(text);
+      button.children.item(0).setAttribute("name","heart");
+      //this.buttonIcon="heart";
+       
+     // card.getElementsByTagName("ion-ripple-effect").item(0).addRipple(1,2);
+
+    } 
+    else {
+      /** sterge textul, mark as unselected **/
+      this.selectedTopics.splice(index, 1);
+      //this.buttonIcon="heart-empty";
+      button.children.item(0).setAttribute("name","heart-empty");
+
+    }
+
+     
+    console.log(this.selectedTopics);
+
+  }
 
 }
