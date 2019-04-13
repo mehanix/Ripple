9a024819a699cb2f7698c26e6a9e6c64a1a155be
Nicoleta Ciausu <nixikunn@gmail.com>
2019-04-13 23:49:13 +0300

    Random lesson loading! Huge news!

--- src/app/main/lesson-select/lesson-select.page.ts
+++ src/app/main/lesson-select/lesson-select.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { FireService } from 'src/app/services/fire.service';
+
+
+export interface Lesson {
+  imagini: string[];
+  data: string[];
+  titlu: string[];
+}
 
 @Component({
   selector: 'app-lesson-select',
@@ -7,9 +16,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonSelectPage implements OnInit {
 
-  constructor() { }
+  private selectedTopic:string;
+  private todaysLesson:Lesson;
+  topicName: string;
+  constructor(private storage:Storage, private fireService:FireService) { 
+
+    this.storage.get('topics').then((val:Array<string>) => {
+      //console.log(val);
+      
+      var topicNr = this.getRandomInt(val.length);
+     
+
+      this.selectedTopic = val[topicNr];
+     // console.log(this.selectedTopic, topicNr);
+     
+
+     var promise =  fireService.selectTopicLesson(this.selectedTopic);
+     promise.then((([array, name])  => { this.todaysLesson = array; this.topicName = name; console.log(val);}));
+    });
+  }
+
+
+
 
   ngOnInit() {
   }
+
+startLesson(){
+
+}
+
+
+getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 }
--- src/app/services/fire.service.ts
+++ src/app/services/fire.service.ts
@@ -2,26 +2,27 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFirestoreCollection, AngularFirestoreDocument, DocumentChangeAction } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { mergeMap, take } from 'rxjs/operators';
 
-export interface Item { 
+
+export interface ItemPrezentare { 
   nume: string;
   imagine: string;
   desc: string;
   id?: string;
 }
 
-
-class ItemData {
-
-  item:Item;
-  selected:boolean;
-  constructor(item:Item, selected:boolean) {
-    this.item = item;
-    this.selected = selected;
-    
-  }
+export interface Topic {
+  nume:string;
+  id:string;
 }
+
+export interface Lesson {
+  imagini: string[];
+  data: string[];
+  titlu: string[];
+}
+
 
 
 @Injectable({
@@ -30,31 +31,31 @@ class ItemData {
 export class FireService {
 
   
-  private topicsPrezentareCollection:AngularFirestoreCollection<Item>;
-  private topicsPrezentare:Observable<Item[]>;
-  private topicsprezentareData:Item[];
-  private selectedItems:ItemData[];
+  private topicsPrezentareCollection:AngularFirestoreCollection<ItemPrezentare>;
+  private topicsPrezentare:Observable<ItemPrezentare[]>;
+  
+  private lessonsCollection: AngularFirestoreCollection<Lesson>;
+  private lessons:Observable<Lesson[]>;
+
+  private todaysLesson:Lesson;
+
+  id:string;
 
   constructor(private firestore: AngularFirestore) { 
 
     /** array cu informatiile de prezentare ale topic-urilor **/
-    this.topicsPrezentareCollection = firestore.collection<Item>('prezentare');
-    this.topicsPrezentare = this.topicsPrezentareCollection.snapshotChanges().pipe(
-      map(actions => actions.map(a => {
-        const data = a.payload.doc.data() as Item;
-        const id = a.payload.doc.id;
-        return { id, ...data };
-      }))
-    );
+  //  this.topicsPrezentare = this.topicsPrezentareCollection.snapshotChanges().pipe(
+   //   map(actions => actions.map(a => {
+   //     const data = a.payload.doc.data() as ItemPrezentare;
+   //     const id = a.payload.doc.id;
+    //    return { id, ...data };
+   //   }))
+   // );
+   this.topicsPrezentareCollection = firestore.collection<ItemPrezentare>('prezentare');
+   this.topicsPrezentare = this.topicsPrezentareCollection.valueChanges();
 
-
-
-    
-     // this.topicsPrezentare.subscribe(changes => {
-     //   return changes;
-
-     //   });
-
+  // this.topicsLessonsCollection = firestore.collection<ItemPrezentare>('topics');
+  // this.topicsLessons = this.topicsLessonsCollection.valueChanges();
 
   }
 
@@ -62,9 +63,65 @@ export class FireService {
     return this.topicsPrezentare;
   }
 
-  public selectPrezentareWithName(name:string)
+  /**** Returneaza colectia cu toate lectiile ****/
+ // public getTopicsLessons() {
+ //   return this.topicsLessons;
+ // }
+
+  /** Functie care alege random o lectie a topicului dat **/
+   public selectTopicLesson(topicName:string)
   {
+    return new Promise((resolve) => {
+      var topics:AngularFirestoreCollection<Topic>= this.firestore.collection('topics', ref => ref.where('nume', '==', topicName))
+   var topicsValue = topics.valueChanges();
+
+   topicsValue.subscribe( ref => ref.map((p) => { 
+
+      this.id = p.id;
+      this.afiseaza(this.id);
+      this.getLessonCollection(this.id);
+
+     // this.lessons.subscribe((val) => console.table(val))
+      var length:number;
+      var lesson = this.lessons.subscribe((val) => {
+        length = val.length;
+        var lessonNumber:number = this.getRandomInt(length);
+        this.todaysLesson = val[lessonNumber];
+       // console.table(this.todaysLesson);
+       resolve([this.todaysLesson,topicName])
+
+      });
+      }))
+      
+      
+      
+    });
+
+
+
+   
+
+      
 
   }
+
+  getTodaysLesson() {
+    return this.todaysLesson;
+  }
   
+
+  afiseaza(value) {
+    console.log(value);
+  }
+
+  getLessonCollection(id:string) {
+    var lessonsDoc:AngularFirestoreDocument<Topic> = this.firestore.doc('topics/'+id);
+    var lessonCollection:AngularFirestoreCollection<Lesson> = lessonsDoc.collection<Lesson>('lectii');
+    this.lessons = lessonCollection.valueChanges();
+  }
+
+  getRandomInt(max:number):number {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
 }
