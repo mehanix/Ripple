1c17d4c276a7f4fa6b56208f9ff37603e0eaefd9
Nicoleta Ciausu <nixikunn@gmail.com>
2019-07-26 03:12:35 +0300

    keeping track of whether lesson was read through or not!

--- src/app/main/lesson-select/lesson-select.page.ts
+++ src/app/main/lesson-select/lesson-select.page.ts
@@ -46,7 +46,7 @@ export class LessonSelectPage implements OnInit {
             
           }).catch(err => {console.log(err)})
     
-      //  }
+   
       }
     })
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -18,7 +18,7 @@ export class DataService {
     img:""
   };*/
 
-  constructor(db:DatabaseService) {
+  constructor() {
     
   
 
